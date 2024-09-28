9a6c01b6f678e13b1a01d8b151ed00dbce6e0112
rembov <i7888@mail.ru>
2024-09-28 22:30:52 +0300

    Исправления

--- src/libsets.h
+++ src/libsets.h
@@ -33,7 +33,7 @@ Sets *Union(Sets **setA, Sets **setB);
 Sets *Difference(Sets **setA, Sets **setB);
 Sets *SymmetricDifference(Sets **setA, Sets **setB);
 Sets *Intersection(Sets **setA, Sets **setB);
-Sets *Complement(Sets **universalSet, Sets **setA) ;
+Sets *Complement(Sets **setA);
 void Push(Sets **tail, int64_t num);
 void Pop(Sets **tail);
 void PrintAll(Sets **tail);
