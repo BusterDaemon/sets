82c2f4b9d9da3b849abca91ec76ea62c65698b24
Buster Daemon <bugsbunnygdi@pm.me>
2024-10-06 06:50:13 +0300

    Небольшие исправления Переименована структура множества Исправлено добавление дубликатов

--- testing/sets_tests_core.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SETS_TESTS_CORE_H
-#define _SETS_TESTS_CORE_H
-#include "../src/libsets.h"
-
-void menuPush(SetElem **A, SetElem **B);
-void menuPop(SetElem **A, SetElem **B);
-void menuPrintSet(SetElem *A, SetElem *B);
-void menuOper(SetElem *A, SetElem *B);
-#endif
